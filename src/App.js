6949831af5f580a3a089ae075320c5643fbab002
KOOMINSEO <seostop32@hotmail.com>
2025-08-06 13:39:26 +0900

    mobile 강제 분기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import MobileToastTest from './components/MobileToastTest';
 import DualOverlayChart from './components/DualOverlayChart'; // 예전 차트
 
 function App() {
-  const isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  // const isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+   const isMobile = true; // ✅ 모바일 강제 분기
   console.log('isMobile:', isMobile, navigator.userAgent);
   return (
     <>
