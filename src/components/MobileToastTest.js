1d5376fb8c3c4e0649ce4effa6f0d7b1347eaa86
KOOMINSEO <seostop32@hotmail.com>
2025-08-06 13:43:25 +0900

    mobile 강제 분기

--- src/components/MobileToastTest.js
+++ src/components/MobileToastTest.js
@@ -1,16 +1,19 @@
+// src/components/MobileToastTest.js
 import React, { useEffect } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const MobileToastTest = () => {
   useEffect(() => {
-    toast.info('모바일 토스트 테스트 알림!', {
-      position: 'bottom-center',
-      autoClose: 3000,
-    });
+    toast.info('📱 모바일 테스트 알림!');
   }, []);
 
-  return <ToastContainer />;
+  return (
+    <div>
+      <h2>📱 MobileToastTest 컴포넌트</h2>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default MobileToastTest;
